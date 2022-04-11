71f1a3714ac337a3ea32dbcde76e6d0ac1e932c9
ma7762la-s <majjalarsen@hotmail.com>
2022-04-11 10:04:49 +0000

    en del av backend

---
+++ HyggeEJBProject/ejbModule/hygge/eao/ics/EducationEAOImpl.java
@@ -0,0 +1,49 @@
+package hygge.eao.ics;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import hygge.ejb.ics.Education;
+
+/**
+ * Session Bean implementation class HyggeEAOImpl
+ */
+@Stateless
+//@LocalBean
+public class EducationEAOImpl implements EducationEAOLocal {
+
+	@PersistenceContext(unitName="HyggeEJBSql") 
+	private EntityManager em; 
+
+	/**
+	 * Default constructor. 
+	 */
+	public EducationEAOImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Education findByEducationName(String educationName) { 
+		return em.find(Education.class, educationName); 
+	} 
+
+	public Education createEducation(Education education) { 
+		em.persist(education); 
+		return education; 
+	} 
+
+	public Education updateEducation(Education education) { 
+		em.merge(education); 
+		return education; 
+	} 
+
+	public void deleteEducation(String educationName) { 
+		Education e = this.findByEducationName(educationName); 
+		if (e != null) { 
+			em.remove(e); 
+		} 
+	} 
+
+
+
+}

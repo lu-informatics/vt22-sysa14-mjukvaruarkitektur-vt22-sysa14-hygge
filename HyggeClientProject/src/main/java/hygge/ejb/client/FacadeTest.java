fcd92a1243d3246f29000ed6a546b5341342f7bf
da1552hu-s <75209747+da1552hu-s@users.noreply.github.com>
2022-05-04 02:04:50 +0200

    junit impl

--- HyggeClientProject/src/main/java/hygge/ejb/client/FacadeTest.java
+++ HyggeClientProject/src/main/java/hygge/ejb/client/FacadeTest.java
@@ -3,13 +3,15 @@ package hygge.ejb.client;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
+import hygge.ejb.ics.Education;
 import hygge.facade.ics.FacadeLocal;
 import junit.framework.TestCase;
 
 public class FacadeTest extends TestCase {
 	FacadeLocal facade;
-	
-	
+	String expectedName, expectedLocale;
+	Education testEducation;
+
 	public FacadeTest(String name) {
 		super(name);
 	}
@@ -18,20 +20,34 @@ public class FacadeTest extends TestCase {
 		super.setUp();
 		Context context = new InitialContext();
 		facade = (FacadeLocal)context.lookup("java:app/HyggeEJBProject/Facade!hygge.facade.ics.FacadeLocal");
+		testEducation = new Education();
+		
+		expectedName = "testLTH";
+		expectedLocale = "testLund";
+		
+		testEducation.setEducationName(expectedName);
+		testEducation.setLocale(expectedLocale);
+		
+		facade.createEducation(testEducation);
 		
 	}
 
 	protected void tearDown() throws Exception {
 		super.tearDown();
+		facade.deleteEducation("testLTH");
 		facade = null;
+		testEducation = null;
+		expectedName = null;
+		expectedLocale = null;
 	}
 
 	public void testFindByEducationName() {
-		assertEquals(facade.findByEducationName("LTH").getLocale(),"Lund");
+		assertTrue(facade.findByEducationName(expectedName).Equals(testEducation));
 	}
 
-	public void testDeleteEducation() {
-		fail("Not yet implemented");
+	public void testUpdateEducation() {
+		testEducation.setLocale("testHelsingborg");
+		facade.updateEducation(testEducation);
+		assertEquals(facade.findByEducationName("testLTH").getLocale(),"testHelsingborg");
 	}
-
 }
